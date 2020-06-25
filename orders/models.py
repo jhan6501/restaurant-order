686a2ec5929c0e765d333f80785c4171f8848e77
Jerry Han <jhan6501@gmail.com>
2020-06-25 13:18:46 -0400

    simple stuff

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Food(models.Model):
+    class Meta: 
+        abstract = True
+
+class Pizza(Food):
+    style = models.CharField(max_length = 64)
+    size = models.CharField(max_length = 64)
+    price = models.FloatField()
+
+    def __str__(self):
+        return f"A {self.size} {self.type} pizza that is {self.toppings} that costs {self.price}"
--- orders/views.py
+++ orders/views.py
@@ -1,6 +1,22 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Pizza
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Project 3: TODO")
+    return render(request, "orders/menu.html")
+
+def orderPizza(request):
+    name = ""
+    foodItem = "Pizza"
+    sizes = ["small", "medium", "large"]
+    types = ["regular", "sicilian"]
+    context = {
+        "food": foodItem,
+        "sizes": sizes,
+        "types": types,
+    }
+
+    return render(request, "orders/orderPizza.html", context)
+
+
